c8f35fc7fae93e27bd62633ce96be88f05497b6c
白夜 <18556134754@163.com>
2023-01-11 00:41:35 +0800

    feat: mysql 更新和删除

--- exercise1/src/mysql/index.js
+++ exercise1/src/mysql/index.js
@@ -24,9 +24,39 @@ const db = mysql.createPool({
 
 // 快捷插入
 // 向表中新增数据时，如果数据对象的每个属性和数据表的字段一一对应，则可以通过以下方式快速插入数据
-const user = { user_name: '王五', password: '123456' };
-const sqlStr = 'insert into users set ?';
-db.query(sqlStr, user, (err, res) => {
+// const user = { user_name: '王五', password: '123456' };
+// const sqlStr = 'insert into users set ?';
+// db.query(sqlStr, user, (err, res) => {
+//   if (err) return console.log(err.message);
+//   console.log(res);
+// });
+
+// 更新
+// const user = { id: 1, user_name: '蝙蝠侠', password: '654321' };
+// const sqlStr = 'update users set user_name=?, password=? where id=?';
+// db.query(sqlStr, [user.user_name, user.password, user.id], (err, res) => {
+//   if (err) return console.log(err.message);
+//   console.log(res);
+// });
+
+// 快捷更新
+// const user = { id: 1, user_name: '钢铁侠', password: '7890-=' };
+// const sqlStr = 'update users set ? where id=?';
+// db.query(sqlStr, [user, user.id], (err, res) => {
+//   if (err) return console.log(err.message);
+//   console.log(res);
+// });
+
+// 物理删除
+// const sqlStr = 'delete from users where id=?';
+// db.query(sqlStr, 2, (err, res) => {
+//   if (err) return console.log(err.message);
+//   console.log(res);
+// });
+
+// 逻辑删除
+const sqlStr = 'update users set status=0 where id=?';
+db.query(sqlStr, 1, (err, res) => {
   if (err) return console.log(err.message);
   console.log(res);
 });
