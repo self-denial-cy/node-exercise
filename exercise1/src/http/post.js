91ce117f673667554ffcf699aaa1909627501919
Scorpio <18556134754@163.com>
2023-02-28 20:02:15 +0800

    feat: post、spider 示例

--- exercise1/src/http/post.js
+++ exercise1/src/http/post.js
@@ -9,25 +9,32 @@ const server = http.createServer((req, res) => {
 });
 
 const fetchData = (callback) => {
-  // Node 可以充当中间层角色，因为不是浏览器环境，没有跨域请求拦截；
-  // 因为调用的是 https 协议的接口，所以必须使用 https 模块
-  https.get('https://i.maoyan.com/api/mmdb/movie/v3/list/hot.json?ct=%E6%98%86%E5%B1%B1&ci=403&channelId=4', (res) => {
+  const options = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    hostname: 'm.xiaomiyoupin.com',
+    port: 443,
+    path: '/mtop/market/search/placeHolder',
+    method: 'POST'
+  };
+  const req = https.request(options, (res) => {
     let data = '';
-    // data 事件监听数据流传输
     res.on('data', (chunk) => {
       console.log('获取数据中');
       console.log(chunk);
       data += chunk;
     });
-    // end 事件监听数据流传输完毕
     res.on('end', () => {
       console.log('数据获取完毕');
       console.log(data);
       callback(data);
     });
   });
+  req.write(JSON.stringify([{}, { baseParam: { ypClient: 1 } }]));
+  req.end();
 };
 
-server.listen(3002, () => {
-  console.log('server running at http://127.0.0.1:3002');
+server.listen(3003, () => {
+  console.log('server running at http://127.0.0.1:3003');
 });
--- exercise1/src/http/spider.js
+++ exercise1/src/http/spider.js
@@ -1,25 +1,32 @@
 const http = require('http');
 const https = require('https');
+const cheerio = require('cheerio');
 
 const server = http.createServer((req, res) => {
   fetchData((data) => {
+    const $ = cheerio.load(data);
+    const $contents = $('.column.content');
+    const movies = [];
+    $contents.each((index, item) => {
+      movies.push({
+        title: $(item).find('.title').text(),
+        grade: $(item).find('.grade').text(),
+        actor: $(item).find('.actor').text()
+      });
+    });
     res.setHeader('Content-Type', 'application/json;charset=utf-8');
-    res.end(data);
+    res.end(JSON.stringify(movies));
   });
 });
 
 const fetchData = (callback) => {
-  // Node 可以充当中间层角色，因为不是浏览器环境，没有跨域请求拦截；
-  // 因为调用的是 https 协议的接口，所以必须使用 https 模块
-  https.get('https://i.maoyan.com/api/mmdb/movie/v3/list/hot.json?ct=%E6%98%86%E5%B1%B1&ci=403&channelId=4', (res) => {
+  https.get('https://i.maoyan.com', (res) => {
     let data = '';
-    // data 事件监听数据流传输
     res.on('data', (chunk) => {
       console.log('获取数据中');
       console.log(chunk);
       data += chunk;
     });
-    // end 事件监听数据流传输完毕
     res.on('end', () => {
       console.log('数据获取完毕');
       console.log(data);
@@ -28,6 +35,6 @@ const fetchData = (callback) => {
   });
 };
 
-server.listen(3002, () => {
-  console.log('server running at http://127.0.0.1:3002');
+server.listen(3005, () => {
+  console.log('server running at http://127.0.0.1:3005');
 });
