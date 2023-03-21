0dd5194c2037f4bd2e4976024fabd0f93f139c09
Scorpio <18556134754@163.com>
2023-03-21 20:32:40 +0800

    feat: 简易版在线聊天室搭建 day3-2

--- practice2/routes/view.js
+++ practice2/routes/view.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const indexRouter = require('./views/index');
 const registerRouter = require('./views/register');
 const loginRouter = require('./views/login');
+const chatRouter = require('./views/chat');
 
 router.use(indexRouter);
 router.use(registerRouter);
 router.use(loginRouter);
+router.use(chatRouter);
 
 module.exports = router;
